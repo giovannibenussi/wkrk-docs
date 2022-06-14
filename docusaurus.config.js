13f8aa322593dae9dcc596415b5f35c66247e928
Giovanni Benussi <giovanni.benussi@usach.cl>
2022-06-14 09:52:53 -0400

    Add trailing slash to baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'wkrk',
   tagline: 'A Simple, Yet Powerful Framework for Cloudflare Workers.',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: process.env.NODE_ENV ==='production' ? '/wkrk-docs' : '/',
+  baseUrl: process.env.NODE_ENV === 'production' ? '/wkrk-docs/' : '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
