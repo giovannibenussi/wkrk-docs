5d94bed3eeda2b67d69a2583f1772ffdabecf3d3
Giovanni Benussi <giovanni.benussi@usach.cl>
2022-06-14 09:51:17 -0400

    Update title, tagline, and baseUrl so it works on production and development

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,10 +6,10 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'wkrk',
+  tagline: 'A Simple, Yet Powerful Framework for Cloudflare Workers.',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/wkrk-docs/',
+  baseUrl: process.env.NODE_ENV ==='production' ? '/wkrk-docs' : '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
